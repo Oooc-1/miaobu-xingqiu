02e06bed0140ee83a77ba970668fe79125cdd050
Oooc <2794860364@qq.com>
2026-06-03 01:10:30 +0800

    实现了相册和照片数据库以及后台管理员的在相关数据库的同步

--- newsApp/admin.py
+++ newsApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import DiaryAlbum, Photo
 
-# Register your models here.
+# 将照片作为相册的“内嵌”部分，这样你在后台添加相册时，就能直接添加照片了
+class PhotoInline(admin.TabularInline):
+    model = Photo
+    extra = 1 # 默认显示1个添加照片的空行
+
+@admin.register(DiaryAlbum)
+class DiaryAlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    inlines = [PhotoInline] # 启用内嵌功能
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'album', 'created_at')
+    list_filter = ('album',) # 可以在侧边栏按相册筛选照片
--- newsApp/models.py
+++ newsApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+# 1. 定义相册模型
+class DiaryAlbum(models.Model):
+    title = models.CharField(max_length=100, verbose_name="相册名称")
+    cover_image = models.ImageField(upload_to='diary/covers/', verbose_name="相册封面")
+    description = models.TextField(blank=True, verbose_name="相册简介")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "追光日记相册"
+        verbose_name_plural = "追光日记相册"
+
+# 2. 定义照片模型
+class Photo(models.Model):
+    # 下面这一整块请确保每个字段名前面都有 4 个空格
+    album = models.ForeignKey(DiaryAlbum, on_delete=models.CASCADE, related_name='photos', verbose_name="所属相册")
+    title = models.CharField(max_length=100, verbose_name="照片标题")
+    image = models.ImageField(upload_to='diary/photos/', verbose_name="日记照片")
+    
+    # 这些是可选字段，确保缩进也是 4 个空格
+    latitude = models.CharField(max_length=20, verbose_name="纬度", blank=True, null=True)
+    longitude = models.CharField(max_length=20, verbose_name="经度", blank=True, null=True)
+    shutter_speed = models.CharField(max_length=20, verbose_name="快门速度", blank=True, null=True)
+    aperture = models.CharField(max_length=20, verbose_name="光圈", blank=True, null=True)
+    iso = models.IntegerField(verbose_name="ISO感光度", blank=True, null=True)
+    
+    # 这一行就是报错的那一行，请特别注意它前面的空格必须和上面的字段完全对齐
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="上传时间")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "拍立得照片"
+        verbose_name_plural = "拍立得照片"
